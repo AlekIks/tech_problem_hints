a8747865a76d6a9c9e66c29d50bd94166e440343
Alexandra Pogrebnaya <ammalium@mail.ru>
2021-04-10 20:17:04 +0300

    mock tests added

---
+++ mock_tests/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cstring>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+using namespace std;
+
+/*****************************************************/
+/*
+ * Исходные классы: Connection и MyBestSiteEverCreated
+ */
+/*****************************************************/
+
+class Connection {
+public:
+    virtual bool login(string& username, string& password) { return true; }
+    virtual bool logout(string& username) { return true;}
+};
+
+class MyBestSiteEverCreated{
+public:
+    explicit MyBestSiteEverCreated(Connection* connection) : connection_(connection) {}
+
+    int Init(string& username, string& password) {
+        if (!connection_->login(username, password)) {
+            cout << "Login Failed\n";
+            return -1;
+        }
+        return 1;
+    }
+private:
+    Connection* connection_;
+};
+
+/*****************************************************/
+/*
+ * TESTING
+ */
+/*****************************************************/
+
+class MockConnection : public Connection{
+public:
+    MockConnection() : Connection() {
+        // дефолтное значение, сам метод вообще не реализован
+        ON_CALL(*this, login(admin_, admin_)).WillByDefault(testing::Return(true));
+    }
+
+    MOCK_METHOD(bool, login, (string& username, string& password), (override));
+private:
+    string admin_ = "admin";
+};
+
+
+class Site : public ::testing::Test {
+};
+
+
+TEST(Site, LoginTest) {
+    std::shared_ptr<MockConnection> mck (new MockConnection);
+
+    // создаем сайт, но от заглушки, а не настоящего Connection
+    MyBestSiteEverCreated site(&*mck);
+
+    string testing_username = "admin";
+    string testing_password = "admin";
+
+    int retValue = site.Init(testing_username, testing_password);
+    // проверяем, что login вызывается
+    EXPECT_CALL(*mck, login).Times(testing::AtLeast(1));
+    // сравниваем значения
+    EXPECT_EQ(retValue, 1);
+}
+
+
+int main(int argc, char* argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
